aa502cc1067a8fd6e89fc49cb99c3855ca7da199
aecf <81099969+albertoecf@users.noreply.github.com>
2021-09-28 07:18:56 -0300

    push no Named from product item to pro detail

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
@@ -13,23 +15,33 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(imageUrl, fit: BoxFit.cover),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (ctx) => ProductDetailScreen(title),
+              ),
+            );
+          },
+          child: Image.network(imageUrl, fit: BoxFit.cover),
+        ),
         footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(
-              icon: Icon(Icons.favorite),
-              color: Theme.of(context).accentColor,
-              onPressed: () {},
-            ),
-            title: Text(
-              title,
-              textAlign: TextAlign.center,
-            ),
-            trailing: IconButton(
-              icon: Icon(Icons.shopping_cart),
-              onPressed: () {},
-              color: Theme.of(context).accentColor,
-            )),
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+        ),
       ),
     );
   }
