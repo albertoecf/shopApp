ad1a582c970c2f686d20a0392c3b7a79870044f8
aecf <81099969+albertoecf@users.noreply.github.com>
2021-10-03 08:52:24 -0300

    L&F cart_screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
 
 class CartScreen extends StatelessWidget {
-  static const routeName = '/cart'; 
-
+  static const routeName = '/cart';
 
   @override
   Widget build(BuildContext context) {
@@ -21,16 +19,26 @@ class CartScreen extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.all(8),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Text(
                     'Total',
                     style: TextStyle(fontSize: 20),
                   ),
-                  SizedBox(width: 10),
+                  Spacer(),
                   Chip(
-                    label: Text('${cart.totalAmount}'),
+                    label: Text(
+                      '${cart.totalAmount}',
+                      style: TextStyle(
+                          color:
+                              Theme.of(context).primaryTextTheme.title.color),
+                    ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
+                  FlatButton(
+                      onPressed: () {},
+                      child: Text('Hacé tu pedido'),
+                      textColor: Theme.of(context).primaryColor)
                 ],
               ),
             ),
